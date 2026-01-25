1adfcef8f1baaf5e1624082821d59232b46a130d
Andre <andrepennini@gmail.com>
2026-01-25 11:55:00 -0300

    teste de favicon e logo

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -106,6 +106,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "src" / "static",  # type: ignore # noqa
 ]
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"  # type: ignore # noqa
+
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 LOGIN_URL = "/accounts/login/"
--- src/config/urls.py
+++ src/config/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,7 +7,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("src.penninicup.urls")),
     path("accounts/", include("src.accounts.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     # Include django_browser_reload URLs only in DEBUG mode
