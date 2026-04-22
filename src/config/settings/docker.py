9d4b33098382718782c265db3b14f3cb7dba61f8
Andre <andrepennini@gmail.com>
2026-04-22 00:08:00 -0300

    Docker config + cryptography access keys

--- src/config/settings/docker.py
+++ src/config/settings/docker.py
@@ -1,5 +1,13 @@
-if IN_DOCKER:  # type: ignore
-    assert MIDDLEWARE[:1] == ["django.middleware.security.SecurityMiddleware"]  # type: ignore[name-defined]
+import os
+
+if IN_DOCKER or os.path.isfile("/.dockerenv"):  # type: ignore # noqa: F821
+    # We need it to serve static files with DEBUG=False
+    assert MIDDLEWARE[:1] == [  # type: ignore # noqa: F821
+        "django.middleware.security.SecurityMiddleware"
+    ]
+    MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # type: ignore # noqa: F821
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 if IN_DOCKER and not DEBUG:  # type: ignore[name-defined]
