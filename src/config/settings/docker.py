4821948ce816cd9d182c8a130b8dcd567344e366
Andre <andrepennini@gmail.com>
2026-04-22 22:54:03 -0300

    Verificação middleware whitenoise resiliente em docker.py

--- src/config/settings/docker.py
+++ src/config/settings/docker.py
@@ -2,10 +2,11 @@ import os
 
 if IN_DOCKER or os.path.isfile("/.dockerenv"):  # type: ignore # noqa: F821
     # We need it to serve static files with DEBUG=False
-    assert MIDDLEWARE[:1] == [  # type: ignore # noqa: F821
-        "django.middleware.security.SecurityMiddleware"
-    ]
-    MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")  # type: ignore # noqa: F821
+    security_middleware = "django.middleware.security.SecurityMiddleware"
+    whitenoise_middleware = "whitenoise.middleware.WhiteNoiseMiddleware"
+    if security_middleware in MIDDLEWARE and whitenoise_middleware not in MIDDLEWARE:  # type: ignore # noqa: F821
+        security_index = MIDDLEWARE.index(security_middleware)  # type: ignore # noqa: F821
+        MIDDLEWARE.insert(security_index + 1, whitenoise_middleware)  # type: ignore # noqa: F821
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
     STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
